2ac0be5232e7c4d30130256e6c222a217c97ecc1
sammoth <sam.griffiths@wazoku.com>
2015-10-29 21:14:09 +0000

    add file as parameter

--- load_config.py
+++ load_config.py
@@ -3,8 +3,8 @@ import json
 __author__ = 'sammoth'
 
 
-def load(app):
-    with open('exchange_config') as fp:
+def load(app, config_file):
+    with open(config_file) as fp:
         config = json.load(fp)
         fp.close()
 
@@ -16,8 +16,13 @@ def load(app):
             app.config['{}.units'.format(exchange)].append(unit)
             for side in config[exchange][unit]:
                 app.config['{}.{}.{}.tolerance'.
-                           format(exchange, unit, side)] = config[exchange][unit][side][
-                    'tolerance']
-                app.config['{}.{}.{}.reward'.
-                           format(exchange, unit, side)] = config[exchange][unit][side][
-                    'reward']
+                           format(exchange,
+                                  unit,
+                                  side)] = config[exchange][unit][side]['tolerance']
+                for tier in ['tier_1', 'tier_2']:
+                    app.config['{}.{}.{}.{}.reward'.
+                               format(exchange,
+                                      unit,
+                                      side,
+                                      tier)] = config[exchange][unit][side][tier]['reward']
+
