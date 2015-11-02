3143a5869b3b9b739119fe0869be3e7ebbdb8c93
sammoth <sam.griffiths@wazoku.com>
2015-11-02 22:01:58 +0000

    update schemas Orders now persist and have a credited flag set.

--- database.py
+++ database.py
@@ -16,12 +16,13 @@ def build(log):
               "address TEXT, exchange TEXT, unit TEXT)")
     log.info('create the orders table')
     c.execute("CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, user TEXT, "
-              "tier TEXT, order_id TEXT, order_amount NUMBER, order_type TEXT, exchange "
-              "TEXT, unit TEXT)")
+              "tier TEXT, order_id TEXT, order_amount NUMBER, side TEXT, exchange "
+              "TEXT, unit TEXT, credited INTEGER)")
     log.info('create the credits table')
     c.execute("CREATE TABLE IF NOT EXISTS credits (id INTEGER PRIMARY KEY, time NUMBER, "
-              "user TEXT, exchange TEXT, unit TEXT, tier TEXT, side TEXT, provided "
-              "NUMBER, total NUMBER, percentage NUMBER, reward NUMBER, paid INTEGER)")
+              "user TEXT, exchange TEXT, unit TEXT, tier TEXT, side TEXT, "
+              "order_id NUMBER, provided NUMBER, total NUMBER, percentage NUMBER, "
+              "reward NUMBER, paid INTEGER)")
     log.info('create the info table')
     c.execute("CREATE TABLE IF NOT EXISTS info (key Text, value Text)")
     c.execute("INSERT INTO info VALUES (?, ?)", ('last_credit_time', 0))
