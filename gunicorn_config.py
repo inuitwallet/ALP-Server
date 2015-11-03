7bf071b3c870d642897638d8ac223ae9beeee44f
sammoth <sam.griffiths@wazoku.com>
2015-11-03 16:23:05 +0000

    start stats collection timer in gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -8,3 +8,5 @@ def on_starting(server):
     pool_server.run_credit_timer()
     # Set the timer for payouts
     pool_server.run_payout_timer()
+    # Set the timer for stats
+    pool_server.run_stats_timer()
